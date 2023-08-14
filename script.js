9e269dc023f31d019fe89d65043d9da61f05326c
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-08-15 00:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 window.promises = [];
 
+for(let i=1;i<=5;i++)
+	{
+		let delay = Math.floor(Math.random()* 5)+1;
+		let prom = new Promise((resolve) => {
+			setTimeout(() => {
+				resolve(`promise ${i} is resolve after a delay of ${delay} seconds`);
+			}, delay*1000);
+		})
+		promises.push(prom);
+	}
+
+Promise.any(promises).then((data) => {
+	let div = document.getElementById("output");
+	div.innerText = data;
+})
 // Do not change the code above this
 // add your promises to the array `promises`
